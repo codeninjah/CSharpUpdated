53c6008e17b957dc05453c9acd3989148bc0bdd7
codeninjah <codeninjahblog@gmail.com>
2017-09-25 12:23:38 +0200

    Skapat Tärningsspelet

---
+++ DiceGame/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    public class Dice
+    {
+		public int NumberOfDots;
+
+		public void Throw()
+		{
+			Random antalPrickar = new Random();
+			NumberOfDots = antalPrickar.Next(1, 6);
+		}
+    }
+}
---
+++ DiceGame/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+	class Game
+	{
+		public bool YouAreAWinner;
+		public void ThrowDices()
+		{
+			Dice DiceOne = new Dice();
+			Dice DiceTwo = new Dice();
+
+			if (DiceOne.NumberOfDots + DiceTwo.NumberOfDots == 7)
+			{
+				YouAreAWinner = true;
+			}
+			else
+				YouAreAWinner = false;
+
+		}
+
+	}
+}
