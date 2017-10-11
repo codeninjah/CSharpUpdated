c8fd70c103592124703c50dba1007ab0635368d7
codeninjah <codeninjahblog@gmail.com>
2017-10-11 10:37:47 +0200

    SIE Test Projektet

---
+++ SIEImporTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SIEImporTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIEImporTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c887d66-2f87-479c-bf4b-126ef86c7e96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SIEImporTests/SIEImportTestLib.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace SIEImporTests
+{
+	[TestClass]
+	public class SIEImportTestLib
+	{
+		[TestMethod]
+		public void CanRead()
+		{
+			Dictionary<string, decimal> accounts = new Dictionary<string, decimal>();
+			var streamReader = File.OpenText(@"SIERedovisning.txt");
+			while(true)
+			{
+				var line = streamReader.ReadLine();
+				if (line == null)
+					break;
+				string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+				var match = Regex.Match(line, pattern);
+				if (match.Success)
+				{
+					var accountId = match.Groups[1].Value;
+					var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+					if (accounts.ContainsKey(accountId))
+						accounts[accountId] += amount;
+					else
+						accounts[accountId] = amount;
+				}
+
+			}
+			//foreach(var entry in accounts.OrderBy(e => e.Key))
+			foreach (var entry in accounts)
+			{
+				//F2 = "Fixed number of decimal points"
+				Debug.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+			}
+
+		}
+	}
+}
